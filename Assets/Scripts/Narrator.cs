1840cd091ba4568e71a8fdb2ff74551576bd5903
Kevin Finn <superkevin@gmail.com>
2019-10-05 14:43:40 -0400

    the ghost says some words

---
+++ Assets/Scripts/Narrator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Narrator : MonoBehaviour
+{
+  public string[] messages;
+  private Nullable<int> currentMessageIndex;
+  private int nextMessageIndex;
+  private float lastMessageStartedAt;
+  private float lastMessageEndedAt;
+
+  void Start()
+  {
+    lastMessageEndedAt = Time.time;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (currentMessageIndex == null)
+    {
+      if (Time.time > lastMessageEndedAt + 5)
+      {
+        lastMessageStartedAt = Time.time;
+
+        GetComponent<Text>().text = messages[nextMessageIndex];
+        currentMessageIndex = nextMessageIndex;
+
+        nextMessageIndex = (nextMessageIndex + 1) % messages.Length;
+      }
+    }
+    else if (Time.time > lastMessageStartedAt + 10 && Input.GetMouseButtonDown(0))
+    {
+      GetComponent<Text>().text = "";
+      currentMessageIndex = null;
+      lastMessageEndedAt = Time.time;
+    }
+  }
+}
---
+++ Assets/Scripts/Voice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Voice : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    void Start()
+    {
+        Update();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetScreenPosition = Camera.main.WorldToScreenPoint(target.position) + offset;
+        GetComponent<RectTransform>().position = new Vector3(
+            targetScreenPosition.x,
+            targetScreenPosition.y,
+            targetScreenPosition.z
+        );
+    }
+}
