b71530872a6ed14b78426a5ad68cffa894d82c5c
Kevin Finn <superkevin@gmail.com>
2019-10-05 01:33:19 -0400

    add a bunch of sprite textures, plus a ghost that moves towards a click

---
+++ Assets/Scripts/MouseFollowing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollowing : MonoBehaviour
+{
+  private Vector3 targetPosition;
+
+  void Start()
+  {
+      targetPosition = transform.position;
+  }
+
+  void FixedUpdate()
+  {
+    if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+    {
+      targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    transform.position = new Vector3(
+        (targetPosition.x + (transform.position.x * 2)) / 3,
+        (targetPosition.y + (transform.position.y * 2)) / 3,
+        transform.position.z
+    );
+  }
+}
