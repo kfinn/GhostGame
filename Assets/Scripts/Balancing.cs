c2963fa1e9bdceff76a6b75edab11d395d4e1b98
Kevin Finn <superkevin@gmail.com>
2019-10-05 15:56:24 -0400

    randomly rotating bike

---
+++ Assets/Scripts/Balancing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balancing : MonoBehaviour
+{
+  private float currentAngle;
+  private float angularVelocity = 10;
+
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    currentAngle += angularVelocity * Time.deltaTime;
+    transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+  }
+
+  public void ApplyForce(float force)
+  {
+    angularVelocity += force;
+  }
+}
---
+++ Assets/Scripts/WindBalanceDisrupting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindBalanceDisrupting : MonoBehaviour
+{
+    float windForce = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        windForce += Random.Range(-10, 10);
+        windForce *= Random.Range(0.5f, 1.2f);
+        windForce = Mathf.Sign(windForce) * Mathf.Min(100, Mathf.Abs(windForce));
+
+        GetComponent<Balancing>().ApplyForce(windForce * Time.deltaTime);
+    }
+}
