899def578c5516d388241f9500f9b22d772319cb
Kevin Finn <superkevin@gmail.com>
2019-10-05 10:35:37 -0400

    ghost animations

---
+++ Assets/Scripts/GhostAnimatorOrienting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostAnimatorOrienting : MonoBehaviour
+{
+    public Animator ghostAnimator;
+
+    void Start()
+    {
+        ghostAnimator.SetFloat("dx", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ghostAnimator.SetFloat("dx", GetComponent<TargetSeeking>().targetPosition.x - transform.position.x);
+    }
+}
