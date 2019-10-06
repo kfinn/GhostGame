1aee94dc2fb887a9fad65529bc366387aa7982a3
Kevin Finn <superkevin@gmail.com>
2019-10-06 11:32:15 -0400

    trucks appear and disappear

---
+++ Assets/Scripts/AppearingAfterWarning.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearingAfterWarning : MonoBehaviour
+{
+    public GameObject toAppear;
+    public GameObject warning;
+
+    private float startedOn;
+    private bool appeared;
+    private bool disappeared;
+
+    void Start()
+    {
+        toAppear.SetActive(false);
+        warning.SetActive(true);
+
+        startedOn = Time.time;
+
+        appeared = false;
+        disappeared = false;
+    }
+
+    void Update()
+    {
+        if (Time.time > startedOn + 3 && !appeared) {
+            warning.SetActive(false);
+            toAppear.SetActive(true);
+            appeared = true;
+        } else if (Time.time > startedOn + 6 && !disappeared) {
+            warning.SetActive(false);
+            toAppear.SetActive(false);
+            disappeared = true;
+        }
+    }
+}
---
+++ Assets/Scripts/HazardBuilding.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardBuilding : MonoBehaviour
+{
+  public GameObject hazard;
+
+  private float lastHazardBuiltAt;
+
+  void Start()
+  {
+    BuildHazard();
+  }
+
+  void Update()
+  {
+    if (Time.time > lastHazardBuiltAt + 10)
+    {
+      BuildHazard();
+    }
+  }
+
+  private void BuildHazard() {
+    var camera = Camera.main;
+    var cameraHalfHeight = camera.orthographicSize;
+    var cameraHalfWidth = cameraHalfHeight * camera.aspect;
+    var x = Random.Range(-cameraHalfWidth, cameraHalfWidth);
+
+    var builtHazard = Instantiate(
+        hazard
+    );
+
+    builtHazard.transform.position = new Vector3(
+        x,
+        builtHazard.transform.position.y,
+        builtHazard.transform.position.z
+    );
+
+    lastHazardBuiltAt = Time.time;
+  }
+}
