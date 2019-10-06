fa27272a09848ee1ac983e2c4dd704d0ac238cd5
Kevin Finn <superkevin@gmail.com>
2019-10-06 12:57:41 -0400

    introduce new hub scene with one trigger, a wrench

---
+++ Assets/Scripts/EnablingAfterTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablingAfterTrigger : MonoBehaviour
+{
+    public GameObject toEnable;
+    public GameObject character;
+
+    void Update()
+    {
+        if (GetComponent<Collider2D>().OverlapPoint(character.transform.position)) {
+            toEnable.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SpinSlowly.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinSlowly : MonoBehaviour
+{
+
+  void Update()
+  {
+    GetComponent<Rigidbody2D>().angularVelocity = -2;
+  }
+}
