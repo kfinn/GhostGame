1568667bd500c4648bea2a47c61e556921b982c8
Kevin Finn <superkevin@gmail.com>
2019-10-06 03:01:35 -0400

    the dog carries the ball

---
+++ Assets/Scripts/Carrying.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrying : MonoBehaviour
+{
+  public GameObject carried;
+  public Vector3 carriedOffset;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    if (carried != null)
+    {
+        var newPosition = transform.position + carriedOffset;
+        carried.GetComponent<Rigidbody2D>().velocity = newPosition - carried.transform.position;
+        carried.transform.position = newPosition;
+    }
+  }
+}
--- Assets/Scripts/Fetching.cs
+++ Assets/Scripts/Fetching.cs
@@ -8,7 +8,6 @@ public class Fetching : MonoBehaviour
   public Transform ballTransform;
   public Transform throwerTransform;
 
-
   void Start()
   {
     GetComponent<Chasing>().target = ballTransform;
@@ -17,22 +16,44 @@ public class Fetching : MonoBehaviour
   void Update()
   {
     var chasing = GetComponent<Chasing>();
-    if (Mathf.Abs(ballTransform.position.x - throwerTransform.position.x) < 2)
+    var carrying = GetComponent<Carrying>();
+    var ballGameObject = ballTransform.gameObject;
+
+    var ballNearThrower = Mathf.Abs(ballTransform.position.x - throwerTransform.position.x) < 2;
+
+    if (ballNearThrower || carrying.carried == ballGameObject)
     {
       chasing.target = throwerTransform;
-      chasing.targetOffsetX = 3;
+
+      var absDistanceToThrower = Mathf.Abs(throwerTransform.position.x - transform.position.x);
+      if (absDistanceToThrower > 1)
+      {
+        var targetOffsetSign = transform.position.x < throwerTransform.position.x ? -1 : 1;
+        chasing.targetOffsetX = targetOffsetSign * 2;
+      }
+
+      if (absDistanceToThrower <= 2)
+      {
+        carrying.carried = null;
+      }
     }
     else
     {
-      if (chasing.target != ballTransform)
-      {
-        chasing.target = ballTransform;
+      chasing.target = ballTransform;
 
-        if (Mathf.Abs(ballTransform.position.x - transform.position.x) > 4)
+      var absDistanceXToBall = Mathf.Abs(ballTransform.position.x - transform.position.x);
+      if (absDistanceXToBall < 0.5)
+      {
+        if (ballTransform.position.y < transform.position.y + 0.5)
         {
-          var targetOffsetSign = transform.position.x < ballTransform.position.x ? 1 : -1;
-          chasing.targetOffsetX = targetOffsetSign * 3;
+          carrying.carried = ballGameObject;
         }
+      }
+      else
+      {
+        var targetOffsetSign = transform.position.x < ballTransform.position.x ? 1 : -1;
+        chasing.targetOffsetX = targetOffsetSign * 4;
+
       }
     }
   }
